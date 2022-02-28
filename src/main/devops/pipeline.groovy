89b2cc9bbcc2638ed25f584e2d3e0e4d0ba0d919
pcarranzac <pcarranzac@falabella.com.pe>
2022-02-28 01:31:27 -0500

    pipeline configuration added 4

--- src/main/devops/pipeline.groovy
+++ src/main/devops/pipeline.groovy
@@ -92,7 +92,7 @@ node {
             sh "cat $BASE_CONFIGMAP"
         }
 
-        dir(SECRET_FOLDER) {
+        /*dir(SECRET_FOLDER) {
             git branch: 'main', credentialsId: GIT_MASTER_CREDENTIALS_ID, url: 'https://github.com/bindord-org/master-secrets.git'
 
             def keyValueProps = [
@@ -104,7 +104,7 @@ node {
             sh "sed -i 's/^/    /' $APPLICATION_PROPERTIES_PATH"
             sh "cat $APPLICATION_PROPERTIES_PATH >> $BASE_CONFIGMAP"
             sh "cat $BASE_CONFIGMAP"
-        }
+        }*/
     }
 
     stage('DEPLOYING CONFIGMAP & SECRETS') {
